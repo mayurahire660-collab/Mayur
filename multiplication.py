8fc573a3ae01b51e79206b8f617811ca281d29f6
Mayur Ahire <mayurahire660@gmail.com>
2026-08-30 18:36:21 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+# Program to multiply two numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+multiplication = a * b
+
+print("Multiplication =", multiplication)
