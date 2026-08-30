40c435e5326c5e6648a396406f1c865dc9d68240
Mayur Ahire <mayurahire660@gmail.com>
2026-08-30 18:35:51 +0530

    Create subtraction.py

---
+++ subtraction.py
@@ -0,0 +1,8 @@
+# Program to subtract two numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+subtraction = a - b
+
+print("Subtraction =", subtraction)
